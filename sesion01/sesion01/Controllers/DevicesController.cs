c3f835917c502f264567bad533e3d72d957ecf00
cezar alejandro ramirez <genomma@live.com>
2015-09-24 10:33:21 -0500

    cambios realizados de prueba

---
+++ sesion01/sesion01/Controllers/DevicesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using sesion01.Models;
+
+namespace sesion01.Controllers
+{
+    public class DevicesController : Controller
+    {
+        private db_tallerEntities db = new db_tallerEntities();
+
+        // GET: Devices
+        public ActionResult Index()
+        {
+            var device = db.Device.Include(d => d.Location);
+            return View(device.ToList());
+        }
+
+        // GET: Devices/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = db.Device.Find(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // GET: Devices/Create
+        public ActionResult Create()
+        {
+            ViewBag.location_id = new SelectList(db.Location, "id_location", "department");
+            return View();
+        }
+
+        // POST: Devices/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_device,name,unam_stock_number,location_id,serial_number")] Device device)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Device.Add(device);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.location_id = new SelectList(db.Location, "id_location", "department", device.location_id);
+            return View(device);
+        }
+
+        // GET: Devices/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = db.Device.Find(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.location_id = new SelectList(db.Location, "id_location", "department", device.location_id);
+            return View(device);
+        }
+
+        // POST: Devices/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_device,name,unam_stock_number,location_id,serial_number")] Device device)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(device).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.location_id = new SelectList(db.Location, "id_location", "department", device.location_id);
+            return View(device);
+        }
+
+        // GET: Devices/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = db.Device.Find(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // POST: Devices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Device device = db.Device.Find(id);
+            db.Device.Remove(device);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ sesion01/sesion01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sesion01.MvcApplication" Language="C#" %>
